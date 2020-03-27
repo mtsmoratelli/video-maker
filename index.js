1f5902c3d0c1230cfb302e7af4186c31ffb89060
Matheus M <mtsmoratelli@gmail.com>
2020-03-27 14:59:59 -0300

    feature: add search term and prefix

---
+++ index.js
@@ -0,0 +1,22 @@
+const readLine = require('readline-sync')
+function start() {
+    const content = {}
+
+    content.searchTerm = askAndReturnSearchTerm()
+    content.prefix = askAndReturnPrefix()
+
+    function askAndReturnSearchTerm() {
+        return readLine.question('Type a Wikepedia search term: ')
+    }
+
+    function askAndReturnPrefix() { 
+        const prefixes = ['Who is', 'What is', 'The history of']
+        const selectedPrefixIndex = readLine.keyInSelect(prefixes, 'Choose one:')
+        const selectedPrefixText = prefixes[selectedPrefixIndex]
+   
+        return selectedPrefixText
+    }
+
+    console.log(content)
+}
+start()
