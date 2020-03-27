b21476f10d762a6a6956cc2ff754257223a77ae0
Matheus M <mtsmoratelli@gmail.com>
2020-03-27 20:33:44 -0300

    video-maker changes

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-const readLine = require('readline-sync')
-function start() {
-    const content = {}
-
-    content.searchTerm = askAndReturnSearchTerm()
-    content.prefix = askAndReturnPrefix()
-
-    function askAndReturnSearchTerm() {
-        return readLine.question('Type a Wikepedia search term: ')
-    }
-
-    function askAndReturnPrefix() { 
-        const prefixes = ['Who is', 'What is', 'The history of']
-        const selectedPrefixIndex = readLine.keyInSelect(prefixes, 'Choose one:')
-        const selectedPrefixText = prefixes[selectedPrefixIndex]
-   
-        return selectedPrefixText
-    }
-
-    console.log(content)
-}
-start()
+const robots = {
+    input: require('./robots/input.js'),
+    text: require('./robots/text.js'),
+    state: require('./robots/state.js'),
+    image: require('./robots/image.js'),
+    video: require('./robots/video.js'),
+    youtube: require('./robots/youtube.js')
+  }
+  
+  async function start() {
+    robots.input()
+    await robots.text()
+    await robots.image()
+   // await robots.video()
+   // await robots.youtube()
+  }
+  
+  start()
